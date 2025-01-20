ba41bb09d0963acbf97c90c688330fc1ed5030d2
saldo27 <luisherrerapara@gmail.com>
2025-01-20 09:38:42 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.checkbox import CheckBox
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
 from kivy.uix.button import Button
