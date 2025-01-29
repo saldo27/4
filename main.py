87fd83a5e4d2b9010b5c40bf66e6fc8a25e1275e
saldo27 <luisherrerapara@gmail.com>
2025-01-29 10:10:02 +0100

    Update main.py

--- main.py
+++ main.py
@@ -346,10 +346,10 @@ class CalendarViewScreen(Screen):
             # Set current_date to the first date in the schedule
             self.current_date = min(self.schedule.keys())
             self.display_month(self.current_date)
-        
-   def display_month(self, date):  # <-- Correct indentation
-    self.calendar_grid.clear_widgets()
-    self.details_layout.clear_widgets()
+
+    def display_month(self, date):  # <-- Correct indentation
+        self.calendar_grid.clear_widgets()
+        self.details_layout.clear_widgets()
         
         # Update month label
         self.month_label.text = date.strftime('%B %Y')
