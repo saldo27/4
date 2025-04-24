207b6fb2deb143b45bbd6627f9fdcaca534b16d7
saldo27 <luisherrerapara@gmail.com>
2025-04-24 21:31:46 +0200

    Update exceptions.py

--- exceptions.py
+++ exceptions.py
@@ -1,3 +1,11 @@
 class SchedulerError(Exception):
     """Custom exception for Scheduler errors"""
     pass
+
+class ConfigError(SchedulerError):
+    """Exception raised for errors in the configuration."""
+    pass
+
+class DataError(SchedulerError):
+    """Exception raised for errors in input data (workers, holidays, etc.)."""
+    pass
